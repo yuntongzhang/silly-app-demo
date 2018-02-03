1fe6794c82ae97dcd1fa5622570252eddfb89def
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2018-02-04 03:16:36 +0800

    fix make max global

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // define global var here
 var app = {
-    
+    max: {
+        x: 0,
+        y: 0,
+        z:0
+    }
 };
 
 window.addEventListener('devicemotion', deviceMotionHandler, false);
@@ -20,17 +24,13 @@ function deviceMotionHandler(eventData) {
 
     var gy = Math.abs(accg.y - acc.y);
     var rgy = Math.floor(gy);
-    var max = {};
-    max.x = 0;
-    max.y = 0;
-    max.z = 0;
 
-    max.x = updateMaxValue(mAcc.x, max.x);
-    max.y = updateMaxValue(mAcc.y, max.y);
-    max.z = updateMaxValue(mAcc.z, max.z);
+    app.max.x = updateMaxValue(mAcc.x, app.max.x);
+    app.max.y = updateMaxValue(mAcc.y, app.max.y);
+    app.max.z = updateMaxValue(mAcc.z, app.max.z);
 
     //display acc readings
-    var accReading = "<p>x: " + mAcc.x + "</br>y: " + mAcc.y + "</br>z: " + mAcc.z + "</br> max: ( " + max.x + ", " + max.y + ", " + max.z + " )"; 
+    var accReading = "<p>x: " + mAcc.x + "</br>y: " + mAcc.y + "</br>z: " + mAcc.z + "</br> max: ( " + app.max.x + ", " + app.max.y + ", " + app.max.z + " )"; 
     document.getElementById('acc-readings').innerHTML = accReading;
 
     //do silly stuff
