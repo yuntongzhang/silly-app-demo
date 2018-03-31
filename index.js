6a898c79c4269e63366cc87dadbec46228c1802d
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2018-03-31 17:13:42 +0800

    debug

--- index.js
+++ index.js
@@ -12,16 +12,13 @@ var app = {
 let sensor = new Accelerometer();
 sensor.start();
 
-sensor.onreading(function () {
+sensor.onreading = () => {
     console.log("Acceleration along X-axis: " + sensor.x);
     console.log("Acceleration along Y-axis: " + sensor.y);
     console.log("Acceleration along Z-axis: " + sensor.z);
-});
+}
 
-sensor.onerror(function (event) {
-    console.log(event.error.name, event.error.message)
-});
-
+sensor.onerror = event => console.log(event.error.name, event.error.message);
 
 window.addEventListener('devicemotion', deviceMotionHandler, false);
 
